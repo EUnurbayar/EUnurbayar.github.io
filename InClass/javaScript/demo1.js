b90094250f6ada51b2e379e2e4f64653c04da024
e.unurbayar83@gmail.com <e.unurbayar83@gamil.com>
2022-05-30 12:30:55 -0500

    update

--- InClass/javaScript/demo1.js
+++ InClass/javaScript/demo1.js
@@ -1,10 +1,21 @@
 console.log("Hello World");
 
-function sumNum(){
-    var j = 9;
-        i =1 ;
-        console.log(j+i);
-}
+// function sumNum(){
+//     var j = 9;
+//     var i = 1;
+//         console.log(j+i);
+// }
 
-sumNum();
-console.log(this);
+// sumNum();
+
+// console.log(this);
+
+// console.log(a);
+// var a = 5;
+
+
+
+b();
+function b(){
+    console.log("b function");
+}
