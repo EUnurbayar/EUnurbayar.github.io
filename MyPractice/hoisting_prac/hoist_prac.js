26d20540653dc19e7f4d7ce5637eae035f6a0655
e.unurbayar83@gmail.com <e.unurbayar83@gamil.com>
2022-06-03 09:06:57 -0500

    practice added

--- MyPractice/hoisting_prac/hoist_prac.js
+++ MyPractice/hoisting_prac/hoist_prac.js
@@ -24,25 +24,66 @@ function f(){
 
 */
 
-x = 1;
-var a = 5;
-var b = 10;
-var c = function(a, b, c){
-    console.log(x);//undefined => first to printout => 1
-    console.log(b); //9 second to print out => 2
-    var f = function(a, b, c){
-        b = a; // 9 cahnged to 8
-       console.log(b);// 8 print out third => 3
-        b = c;// 8 is changed to => 10.
-        var x = 5;
-    }
-    f(a,b,c);
-    console.log(b);//9 print out => 4
-    var x = 10;
+// x = 1;
+// var a = 5;
+// var b = 10;
+// var c = function(a, b, c){
+//     console.log(x);//undefined => first to printout => 1
+//     console.log(b); //9 second to print out => 2
+//     var f = function(a, b, c){
+//         b = a; // 9 cahnged to 8
+//        console.log(b);// 8 print out third => 3
+//         b = c;// 8 is changed to => 10.
+//         var x = 5;
+//     }
+//     f(a,b,c);
+//     console.log(b);//9 print out => 4
+//     var x = 10;
+// }
+// c(8,9,10);
+// console.log(b);// 10 print out => 5
+// console.log(x);// 1 print out => 6
+
+
+//example1
+function a(){
+    console.log(x);
 }
-c(8,9,10);
-console.log(b);// 10 print out => 5
-console.log(x);// 1 print out => 6
 
+function b(){
+    var x =10;
+    a();
+}
 
+var x =20;
+b();
 
+//example2
+
+function B(){
+    function A(){
+        console.log(x)
+    }
+    var x = 10;
+    A();
+}
+var x =20;
+B();
+
+//example3
+function f(){
+    var a=1, b=20, c;
+    console.log(a + " " + b +" "+ c);
+    function g(){
+        var b = 300, c = 4000;
+        console.log(a + " " + b +" "+ c);
+        a = a + b + c;
+        console.log(a + " " + b +" "+ c);
+
+    }
+    console.log(a + " " + b +" "+ c);
+    g();
+    console.log(a + " " + b +" "+ c);
+}
+
+f();
