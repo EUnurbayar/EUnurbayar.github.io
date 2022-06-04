55398adc916c67eb238b2a4418a51ba13b1fd2d5
e.unurbayar83@gmail.com <e.unurbayar83@gamil.com>
2022-06-04 12:35:38 -0500

    added a mid exam review practicec 1st part

---
+++ MyPractice/MIdExam-review/script/midExam_rev.js
@@ -0,0 +1,25 @@
+/**
+ * midExam.html runnig
+ */
+
+"Use strict"
+console.log("hello script")
+
+var x = 9;
+console.log(myFunction());
+function myFunction(){
+    return x*x;
+}
+console.log(myFunction());
+x = 5;
+console.log(myFunction());
+
+
+// var foo = 1;
+// function bar(){
+//     if(!foo){
+//         var foo = 2;
+//     }
+//     alert(foo);
+// }
+// bar();
