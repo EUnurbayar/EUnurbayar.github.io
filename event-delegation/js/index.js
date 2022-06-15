2f71131855a8e8fcfa3f23e09e8aa137bd01893f
e.unurbayar83@gmail.com <e.unurbayar83@gamil.com>
2022-06-15 09:55:07 -0500

    add

--- event-delegation/js/index.js
+++ event-delegation/js/index.js
@@ -2,31 +2,3 @@
  * index.js
  */
 
-
-$("#articlesList ").on("click", "article",function(event){
-    event.preventDefault();
-    console.log($(this).text());
-    const $a = $(`
-   
-        <p>Article Title - 
-            <a href="http://www.google.com">Click to Add a new Article</a>
-        </p>
-   `)
-    $("#articlesList ").append($a)
-});
-
-
-
-
-// const customUI = document.createElement("article");
-// function responding(event){
-//     if(event.target.nodeName == " article"){
-//         console.log("responding")
-//     }
-//     for(let i = 1; i < 10; i++){
-//         const newElement = document.createElement("article");
-//         newElement.textContent = i;
-//         customUI.append(newElement);
-//     }
-// }
-// customUI.addEventListener("click", responding);
